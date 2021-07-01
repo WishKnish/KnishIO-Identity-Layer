6c2a4afb7178ed0a3f909ca683e0d251c8704ccf
Vladimir Makarov <vladimir.php@gmail.com>
2021-07-01 15:02:07 +0300

    Changes with new identity implementation

--- src/boot/identity.js
+++ src/boot/identity.js
@@ -1,37 +1,78 @@
-import User from '@wishknish/knishio-identity-layer/src/User';
 import KnishIOVuexModel from '@wishknish/knishio-identity-layer/src/KnishIOVuexModel';
+import User from '@wishknish/knishio-identity-layer/src/User';
 import UserWallets from '@wishknish/knishio-identity-layer/src/UserWallets';
+
+import Vuex from 'vuex';
 
 /**
  *
  */
-export default class identity {
+export default class Identity {
 
 
   /**
-   *
-   * @param options
-   * @returns {{}}
+   * Get all computed fields
+   * @returns {*}
    */
-  static fillStore( options = {} ) {
-
-    options.state = {};
-    options.getters = {};
-    options.mutations = {};
-
-    User.fillVuexStorage( options );
-    UserWallets.fillVuexStorage( options );
-
-    return options;
+  static getComputed() {
+    return Object.assign( User.vuexModel.getComputed() , UserWallets.vuexModel.getComputed() );
   }
 
 
   /**
    *
+   * @param Vue
+   * @param userAttributes
+   */
+  constructor( Vue, userAttributes ) {
+    this.init( Vue, userAttributes );
+  }
+
+
+  /**
+   *
+   * @param Vue
    * @param userAttributes
    * @param logging
    */
-  static initVuexModels( store, userAttributes, logging = false ) {
+  init( Vue, userAttributes, logging ) {
+
+    // Create a vuex store object
+    this.$__store = this.createStore( Vue );
+
+    // Init vuex models for User & UserWallets
+    this.initVuexModels( this.$__store, userAttributes, logging );
+  }
+
+
+  /**
+   *
+   * @param Vue
+   * @returns {Store<unknown>}
+   */
+  createStore( Vue ) {
+    Vue.use( Vuex );
+
+    // Init store options
+    let options = {
+      state: {},
+      getters: {},
+      mutations: {},
+    };
+    User.fillVuexStorage( options );
+    UserWallets.fillVuexStorage( options );
+
+    return new Vuex.Store( options );
+  }
+
+
+  /**
+   *
+   * @param store
+   * @param userAttributes
+   * @param logging
+   */
+  initVuexModels( store, userAttributes, logging = false ) {
 
     // Create a user vuex model from the base model class without KnishIOClient
     User.vuexModel = new KnishIOVuexModel(
